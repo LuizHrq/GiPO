00d7e42ddd492f59e51ac90142905e037087210f
Luiz Nascimento <luizhenriquensc.gmail.com>
2023-05-18 20:16:24 -0300

    upgrade v1.2

--- src/Componentes/Fase1/QuizFase1/QuizFase1.js
+++ src/Componentes/Fase1/QuizFase1/QuizFase1.js
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 import './QuizFase1.css';
+import { FaArrowRight } from 'react-icons/fa';
 
-function Quiz1() {
+export default function Quiz1() {
   const [answer, setAnswer] = useState('');
   const [isCorrect, setIsCorrect] = useState(false);
   const [showResult, setShowResult] = useState(false); // nova variável de estado para controlar a exibição da mensagem de resposta
+
+  const pergunta = 'O que é um IPO?';
+  const alternativa1 = 'A - Declaração de falência de uma companhia.';
+  const alternativa2 = 'B - Investir em ações na Bolsa de Valores do Brasil.';
+  const alternativa3 = 'C - Abertura do capital de valores de uma companhia na Bolsa de Valores do Brasil.';
+  const alternativa4 = 'D - Todas as alternativas estão corretas.'
 
   const handleAnswerChange = (e) => {
     setAnswer(e.target.value);
@@ -12,7 +19,7 @@ function Quiz1() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (answer === 'alternativa correta') {
+    if (answer === 'alternativa-correta') {
       setIsCorrect(true);
     } else {
       setIsCorrect(false);
@@ -20,38 +27,57 @@ function Quiz1() {
     setShowResult(true);
   };
 
+
+  const nextFase = (e) => {
+    e.preventDefault();
+    if( isCorrect === true){
+      window.location.href = "/fase2";
+    }
+  }
+
+
   return (
-    <div className='containerquiz'>
-        <div className='cabecalhoteste'>
-            <h1>Teste seu conhecimento:</h1>
+    <>
+      <div className='containerquiz'>
+          <div className='cabecalhoteste'>
+              <h1>Teste seu conhecimento:</h1>
+          </div>
+          <p>Mediante os conteúdos abordados, responda: <br></br>{pergunta}</p>
+
+          <form className='formularioquiz' onSubmit={handleSubmit}>
+              <label>
+                  <input type="radio" value="alternativa1" checked={answer === 'alternativa1'} onChange={handleAnswerChange} />
+                  {alternativa1}
+                  </label>
+              <label>
+                  <input type="radio" value="alternativa2" checked={answer === 'alternativa2'} onChange={handleAnswerChange} />
+                  {alternativa2}
+              </label>
+              <label>
+                  <input type="radio" value="alternativa-correta" checked={answer === 'alternativa-correta'} onChange={handleAnswerChange} />
+                  {alternativa3}
+              </label>
+              <label>
+                  <input type="radio" value="alternativa4" checked={answer === 'alternativa4'} onChange={handleAnswerChange} />
+                  {alternativa4}
+              </label>
+              <br />
+
+              <button type="submit">Verificar resposta</button>
+          </form>
+          {showResult && isCorrect && <p className="quiz-result correct">Parabéns, sua resposta está correta!</p>}
+          {showResult && !isCorrect && <p className="quiz-result incorrect">Resposta incorreta.</p>}
+      </div>
+      
+
+      {showResult && (
+        <div className="proxima-fase">{isCorrect && (
+          <button onClick={nextFase}><FaArrowRight/><br></br><span>Próxima fase</span>
+          </button>)}
         </div>
-      <p>Mediante os conteúdos abordados, responda: <br></br>O que é um IPO? </p>
+      )}
 
-        <form className='formularioquiz' onSubmit={handleSubmit}>
-            <label>
-                <input type="radio" value="alternativa 1" checked={answer === 'alternativa 1'} onChange={handleAnswerChange} />
-                A - Declaração de falência de uma companhia.
-                </label>
-            <label>
-                <input type="radio" value="alternativa 2" checked={answer === 'alternativa 2'} onChange={handleAnswerChange} />
-                B - Investir em ações na Bolsa de Valores do Brasil.
-            </label>
-            <label>
-                <input type="radio" value="alternativa correta" checked={answer === 'alternativa correta'} onChange={handleAnswerChange} />
-                C - Abertura do capital de valores de uma companhia na Bolsa de Valores do Brasil.
-            </label>
-            <label>
-                <input type="radio" value="alternativa 4" checked={answer === 'alternativa 4'} onChange={handleAnswerChange} />
-                D - Todas as alternativas estão corretas.
-            </label>
-            <br />
+    </>
 
-            <button type="submit">Verificar resposta</button>
-        </form>
-        {showResult && isCorrect && <p className="quiz-result correct">Parabéns, sua resposta está correta!</p>}
-        {showResult && !isCorrect && <p className="quiz-result incorrect">Resposta incorreta.</p>}
-    </div>
   );
 }
-
-export default Quiz1;
